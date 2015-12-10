de1b97186d32dfc1c8aa8dba59cf8313efd30c69
Mahmoud Saleh <mmd.sh@Mahmouds-MacBook-Pro.local>
2015-12-10 18:44:11 -0500

    Added more notes

--- fifth.m
+++ fifth.m
@@ -1,10 +1,8 @@
 clear
 notecreate = @(frq,dur) sin(2*pi* [0:dur]/8192 * (220*2.^((frq-1)/12)));
 notename = {'A' 'A#' 'B' 'C' 'C#' 'D' 'D#' 'E' 'F' 'F#' 'G' 'G#'};
-'Z'== sin(2*pi*0*(0:0.000125:0.5));%pause
-%l1 = {'G' 'G' 'G' 'E'};
-song = { 'G' 'G' 'G' 'E'};
-% =[l1 ,l2];
+rest = sin(2*pi*0*(0:0.000125:0.03));
+song = {'G' 'G' 'G' 'E' 'F' 'F' 'F' 'D' 'G' 'G' 'G' 'E' 'A' 'A' 'A' 'A' 'A' 'A'};
 for k1 = 1:length(song)
     idx = strcmp(song(k1), notename);
     songidx(k1) = find(idx);
@@ -13,7 +11,6 @@ dur = 0.3*9000;
 songnote = [];
 for k1 = 1:length(songidx)
     songnote = [songnote; [notecreate(songidx(k1),dur)  zeros(1,75)]'];
-    
 end
 sound(songnote)
 
