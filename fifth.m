ff38bf15d0ee02a0fb2fc6fc4f56f3132fb307e9
Mahmoud Saleh <mmd.sh@Mahmouds-MacBook-Pro.local>
2015-12-10 19:13:38 -0500

    Frequency Change

--- fifth.m
+++ fifth.m
@@ -1,5 +1,5 @@
 clear
-notecreate = @(frq,dur) sin(2*pi* [0:dur]/8192 * (220*2.^((frq-1)/12)));
+notecreate = @(frq,dur) sin(2*pi* [0:dur]/8192 * (440*2.^((frq-1)/12)));
 notename = {'A' 'A#' 'B' 'C' 'C#' 'D' 'D#' 'E' 'F' 'F#' 'G' 'G#'};
 rest = sin(2*pi*0*(0:0.000125:0.03));
 song = {'G' 'G' 'G' 'E' 'F' 'F' 'F' 'D' 'G' 'G' 'G' 'E' 'A' 'A' 'A' 'A' 'A' 'A'};
@@ -14,3 +14,4 @@ for k1 = 1:length(songidx)
 end
 sound(songnote)
 
+%% need to add a rest / pause between notes 
