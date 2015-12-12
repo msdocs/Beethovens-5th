529c0d8bbb21df3d2662fbb4f81952181f58092a
Mahmoud Saleh <msdocs@me.com>
2015-12-12 18:12:58 -0500

    Unploted [Complete]
    Project Currently
    * Generates Beethoven’s 5th
    * Generates Noise
    *Adds Gaussian Noise
    * Filters Noise
    * Documentation Halted

--- fifth.m
+++ fifth.m
@@ -1,20 +1,36 @@
 clear
-%Generate Song 
-notecreate = @(frq,dur) sin(2*pi* [0:dur]/8192 * (220*2.^((frq-1)/12)));
+%Generate Notes For Bethoveen's 5th @ 440Hz 
+notecreate = @(frq,dur) sin(2*pi* [0:dur]/8192 * (440*2.^((frq-1)/12)));
 notename = {'A' 'A#' 'B' 'C' 'C#' 'D' 'D#' 'E' 'F' 'F#' 'G' 'G#'};
 rest = sin(2*pi*0*(0:0.000125:0.03));
-song = {'G' 'G' 'G' 'E' 'F' 'F' 'F' 'D' 'G' 'G' 'G' 'E' 'A' 'A' 'A' 'A' 'A'};
+%Generating Beethoven's 5th 
+song = {'G' 'G' 'G' 'E' 'F' 'F' 'F' 'D' 'G' 'G' 'G' 'E' 'A' 'A'};
 for k1 = 1:length(song)
     idx = strcmp(song(k1), notename);
-    songidx(k1) = find(idx);
+    songidx(k1) = find(idx);    
 end    
 dur = 0.3*9000;
 songnote = [];
 for k1 = 1:length(songidx)
     songnote = [songnote; [notecreate(songidx(k1),dur)  zeros(1,75)]'];
 end
+%For Sound Debugging 
 %sound(songnote)
+%Writing Sound To .Wav File
 wavwrite(songnote,'5th.wav')
-
-noisyVoice = awgn(signal,10)
-
+%plot sound 
+%Adding Gausian Noise 
+noisySound = songnote + 0.01 * randn(length(songnote),1)
+wavwrite(noisySound,'noise-5th.wav');
+%plot withnoise 
+%Filter Noise 
+clear 
+[f,fs] = wavread('noise-5th.wav');
+audios=audioplayer(f,fs);
+%Design a bandpass filter that filters out between 700 to 12000 Hz
+n = 1;
+beginFreq = 200 / (fs/2);
+endFreq = 220 / (fs/2);
+[b,a] = butter(n, [beginFreq, endFreq], 'bandpass');
+filterOut = filter(b, a, f);
+wavwrite(filterOut, fs,'denoised.wav');
